a799a438c8197312350b91ab7a6a475b89a5e4d6
Denys Leon <leonden0046@gmail.com>
2023-08-23 23:59:56 +0300

    Selection sort, Bubble sort, Insertion sort

---
+++ DataStructuresAndAlgorithms/CustomDataTypeCollection/CArray.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.CustomDataTypeCollection
+{
+    public class CArray
+    {
+        private int[] array;
+        private int upper;
+        private int numberOfElemets;
+
+        public CArray(int size)
+        {
+            array = new int[size];
+            upper = size - 1;
+            numberOfElemets = 0;
+        }
+
+        public void Insert(int element)
+        {
+            array[numberOfElemets] = element;
+            numberOfElemets++;
+        }
+
+        public void DisplayElements()
+        {
+            for (int i = 0; i < upper; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < upper; i++)
+            {
+                array[i] = 0;
+            }
+
+            numberOfElemets = 0;
+        }
+
+        public void BubbleSort()
+        {
+            int temp;
+            for (int outer = upper; outer >= 1; outer--)
+            {
+                for (int inner = 0; inner < outer - 1; inner++)
+                {
+                    if (array[inner] > array[inner + 1])
+                    {
+                        temp = array[inner];
+                        array[inner] = array[inner + 1];
+                        array[inner + 1] = temp;    
+                    }
+                }
+                this.DisplayElements();
+                Console.WriteLine();
+            }
+        }
+
+        public void SelectionSort()
+        {
+            int temp;
+            int min; // index of minimum element
+
+            for (int outer = 0; outer <= upper; outer++)
+            {
+                min = outer;
+
+                for (int inner = outer + 1; inner <= upper; inner++)
+                {
+                    if (array[inner] < array[min])
+                    {
+                        min = inner;
+                    }
+                }
+
+                temp = array[min];
+                array[min] = array[outer];
+                array[outer] = temp;
+            }
+        }
+
+        public void InsertionSort()
+        {
+            int temp;
+            int inner;
+
+            for (int outer = 1; outer <= upper; outer++)
+            {
+                temp = array[outer];
+                inner = outer;
+
+                while (inner > 0 && array[inner - 1] >= temp)
+                {
+                    array[inner] = array[inner - 1];
+                    inner -= 1;
+                }
+
+                array[inner] = temp;
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -3,111 +3,26 @@ using System;
 
 namespace DataStructuresAndAlgorithms
 {
-    public class Test
-    {
-        public string StudentName { get; set; }
-        public int TestNumber { get; set; }
-
-        public Test(string studentName, int testNumber)
-        {
-            StudentName = studentName;
-            TestNumber = testNumber;
-        }
-    }
-
     public class Program
     {
-        static Collection submittedTests = new Collection();
-        static Collection outForCheckingTests = new Collection();
-
         static void Main(string[] args)
         {
-            int choice;
+            CArray nums = new CArray(6);
 
-            while (true)
+            var random = new Random();
+
+            for (int i = 0; i <= 5; i++)
             {
-                choice = int.Parse(Console.ReadLine());
-
-                switch (choice)
-                {
-                        case 1:
-                        TurnInTest();
-                        break;
-                        case 2:
-                        LookAtTest();
-                        break;
-                        case 3:
-                        ReturnTest();   
-                        break;
-                        case 4:
-                        Exit();
-                        break;
-                }
-            }
-        }
-
-        private static void TurnInTest()
-        {
-            Console.WriteLine("Enter creds: ");
-            Console.WriteLine("Name: ");
-            string studentName = Console.ReadLine();
-            Console.WriteLine("Test number: ");
-            int testNumber = int.Parse(Console.ReadLine());
-
-            Test test = new Test(studentName, testNumber);
-            submittedTests.Add(test);
-        }
-
-        private static void LookAtTest()
-        {
-            int testNumber = int.Parse(Console.ReadLine());
-
-            foreach (Test test in submittedTests)
-            {
-                if(test.TestNumber == testNumber)
-                {
-                    submittedTests.Remove(test);
-                    outForCheckingTests.Add(test);
-                    Console.WriteLine("Test is found");
-                    return;
-                }
+                nums.Insert(random.Next(10,100));
             }
 
-            Console.WriteLine("Test is not found");
-        }
+            nums.DisplayElements();
 
-        private static void ReturnTest()
-        {
-            int testNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sorted: ");
 
-            foreach (Test test in submittedTests)
-            {
-                if (test.TestNumber == testNumber)
-                {
-                    submittedTests.Add(test);
-                    outForCheckingTests.Remove(test);
-                    Console.WriteLine("Test is returned");
-                    return;
-                }
-            }
+            nums.InsertionSort();
 
-            Console.WriteLine("Test is not found");
-        }
-
-        private static void Exit()
-        {
-            foreach (Test outForCheckingTest in outForCheckingTests)
-            {
-                submittedTests.Add(outForCheckingTest);
-            }
-
-            outForCheckingTests.Clear();
-
-            Console.WriteLine("Submitted tests: ");
-            foreach (Test submittedTest in submittedTests)
-            {
-                Console.WriteLine(submittedTest.StudentName + " | " + submittedTest.TestNumber);
-            }
+            nums.DisplayElements();
         }
     }
 }
